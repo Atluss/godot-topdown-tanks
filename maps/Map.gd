0566212f3d91c0b6e74dbebc280ea63d0b1ce242
Shokin Ilya <atlus3@mail.ru>
2018-10-05 15:30:31 +0300

    add obsticls

--- maps/Map.gd
+++ maps/Map.gd
@@ -17,3 +17,7 @@ func _on_Tank_shoot(bullet, _position, _direction):
 	var b = bullet.instance()
 	add_child(b)
 	b.start(_position, _direction)
+
+func _on_Player_dead():
+	get_tree().reload_current_scene()
+	pass # replace with function body
