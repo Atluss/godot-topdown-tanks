1efeeb5986c25fe3cc616def6b82e7150ac42276
Shokin Ilya <atlus3@mail.ru>
2018-10-23 17:02:22 +0300

    ammo

--- environment/Pickup.gd
+++ environment/Pickup.gd
@@ -2,9 +2,14 @@ extends Area2D
 
 enum Items {health, ammo}
 
+var icon_texture = [preload("res://assets/effects/wrench_white.png"), preload("res://assets/effects/ammo_machinegun.png")]
+
 export (Items) var type = Items.health
 export (Vector2) var amount = Vector2(10, 25)
 
+func _ready():
+	$Icon.texture = icon_texture[type]
+	pass
 
 func _on_Pickup_body_entered(body):
 	match type:
@@ -13,5 +18,5 @@ func _on_Pickup_body_entered(body):
 				print("healing")
 				body.heal(int(rand_range(amount.x, amount.y)))
 		Items.ammo:
-			pass
+			body.ammo += int(rand_range(amount.x, amount.y))
 	queue_free()
