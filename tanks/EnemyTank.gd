abd875e9b1d2feecba03e777dcb5ce5fc1340ac1
Shokin Ilya <atlus3@mail.ru>
2018-12-04 17:50:11 +0300

    object rotation enemy

--- tanks/EnemyTank.gd
+++ tanks/EnemyTank.gd
@@ -26,19 +26,41 @@ func control(delta):
 		if target:
 			velocity = Vector2()
 			var rot_dir = 0
-			var target_dir = (target.global_position - global_position).normalized()
-			var current_dir = Vector2(1, 0).rotated(global_rotation)
-			global_rotation = current_dir.linear_interpolate(target_dir, rotation_speed * delta).angle()
-			if abs(target_dir.dot(current_dir)) > 0.4:
-				speed = lerp(speed, max_speed, 0.01)
-				rot_dir += 1
-			else:
-				speed = lerp(speed, 0, 1)
-				rot_dir -= -1
-			if $LookAhead1.is_colliding() or $LookAhead2.is_colliding():
-				speed = lerp(speed, 0, .7)
-			velocity += Vector2(speed, 0).rotated(global_rotation * rot_dir)
+			var collider = null
 			
+			if $LookAhead1.is_colliding() or $LookAhead2.is_colliding():
+				speed = lerp(speed, 0, 1)
+				if $LookAhead1.is_colliding() and $LookAhead2.is_colliding():
+					speed = lerp(speed, -max_speed/2, 0.10)
+				elif $LookAhead1.is_colliding():
+					collider = $LookAhead1.get_collider()
+					rot_dir -= 1
+				elif $LookAhead2.is_colliding():
+					collider = $LookAhead2.get_collider()
+					rot_dir += 1
+
+				if collider != null:
+					var target_dir = (target.global_position + global_position).normalized()
+					var current_dir = Vector2(1, 0).rotated(global_rotation)
+					global_rotation = current_dir.linear_interpolate(target_dir, rotation_speed * delta).angle()
+					velocity += Vector2(speed, 0).rotated(global_rotation * rot_dir)
+				else:
+					rotation += rotation_speed * rot_dir * delta
+					velocity += Vector2(speed, 0).rotated(rotation)
+			else:
+				var target_dir = (target.global_position - global_position).normalized()
+				var current_dir = Vector2(1, 0).rotated(global_rotation)
+				global_rotation = current_dir.linear_interpolate(target_dir, rotation_speed * delta).angle()
+				if abs(target_dir.dot(current_dir)) > 0.4:
+					speed = lerp(speed, max_speed, 0.01)
+					rot_dir += 1
+				else:
+					speed = lerp(speed, 0, 1)
+					rot_dir -= -1
+					
+				velocity += Vector2(speed, 0).rotated(global_rotation * rot_dir)
+		else:
+			speed = lerp(speed, 0, 0.7)
 	pass
 
 func _process(delta):
